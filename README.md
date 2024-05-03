d6ebc9fb350d72ab8a540ca1983223f068f49509
Alaa-CW <ala.tahhan.cw@gmail.com>
2024-05-03 22:44:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# Weather App by Alaa Tahhan
+
+![Weather App Banner](src/assets/WeatherAppScreenshot.png)
+
+This is a simple weather app that allows users to search for cities and view the current weather as well as a short forecast for the upcoming days.<br>
+[Click Here to View Demo](https://weatherapp-alaa.netlify.app/)
+
+## Features
+
+- 🌐 **Live Weather Data:** Fetches real-time weather information based on the user's geolocation.
+- 🔄 **Dynamic Icons:** Displays weather icons that change based on the weather conditions and time of day.
+- 🌡️ **Temperature Conversion:** Shows temperatures in Celsius, converted from Kelvin on the fly.
+- ⚠️ **Error Handling:** Robust error management for location services and API requests.
+- 📱 **Responsive Design:** Optimized for different devices, ensuring a seamless user experience across platforms.
+
+## Tech Stack
+
+### Frontend Technologies
+
+<div align="left">
+
+<table>
+  <tr>
+    <td align="center"><img src="https://cdn.simpleicons.org/react/61DAFB" width="40" height="40"><br>React</td>
+    <td align="center"><img src="https://cdn.simpleicons.org/css3/1572B6" width="40" height="40"><br>CSS</td>
+  </tr>
+</table>
+
+</div>
+
+### APIs
+
+|                                                                                                                           | Title          | Description                                                        |
+| ------------------------------------------------------------------------------------------------------------------------- | -------------- | ------------------------------------------------------------------ |
+| <img src="https://avatars.githubusercontent.com/u/1743227?s=280&v=4" width="50" height="auto">                            | OpenWeatherMap | Fetches weather data including temperature, conditions, and icons. |
+| <img src="https://rapidapi-prod-apis.s3.amazonaws.com/099e863e-e079-449d-867c-fe5d87eb668c.png" width="40" height="auto"> | API Ninjas     | Retrieves the city name based on latitude and longitude.           |
+
+### Tools and Libraries
+
+|                                                                             | Title | Description                                                     |
+| --------------------------------------------------------------------------- | ----- | --------------------------------------------------------------- |
+| <img src="https://cdn.simpleicons.org/axios/5A29E9" width="40" height="40"> | Axios | Promise-based HTTP client for making requests to external APIs. |
+
+## Setup and Installation
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/SoftwareEngineerAlaa/weather-app.git
+   ```
+2. **Navigate to the project directory:**
+   ```bash
+   cd weather-app
+   ```
+3. **Install dependencies:**
+   ```bash
+   npm install
+   ```
+4. **Run the application:**
+   ```bash
+   npm start
+   ```
+   This will run the app in development mode. Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Usage
+
+Simply open the app, and it will request permission to access your location. Once permitted, it will automatically fetch and display the weather data for your current location. You can see the current temperature, weather conditions, and minimum/maximum temperatures for the day.
+
+## Possible Developments
+
+- 🔍 Search bar for finding weather in different cities.
+- 📅 Short weather forecast for eacn h day of the week.
+- 🌟 Animated SVG for a more user-friendly interface.
+- 🌍 Support for multiple languages such as Arabic, German, French, Spanish, Italian, Japanese, Turkish, etc.
+- 🎯 Suggestions for activities based on the weather conditions.
+
+## Contribution
+
+Contributions to app development are welcome. Contributors retain copyright to their contributions, while others are granted the right to use, modify, and distribute the project under the MIT License.
+
+## Acknowledgements
+
+- **OpenWeatherMap API:** For providing comprehensive weather data.
+- **API Ninjas:** For enabling accurate reverse geocoding features.
+
+## Developer Contact
+
+**Alaa Tahhan**
+
+- [Portfolio](https://alaatahhan.netlify.app/)
+- [GitHub](https://github.com/SoftwareEngineerAlaa)
+- [LinkedIn](https://www.linkedin.com/in/alaa-tahhan/)
+
+Thank you for exploring the Weather App!
---
+++ src/App.js
@@ -0,0 +1,242 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import img01d from "./assets/01d.svg";
+import img01n from "./assets/01n.svg";
+import img02d from "./assets/02d.svg";
+import img02n from "./assets/02n.svg";
+import img03d from "./assets/03d.svg";
+import img03n from "./assets/03n.svg";
+import img04d from "./assets/04d.svg";
+import img04n from "./assets/04n.svg";
+import img09d from "./assets/09d.svg";
+import img09n from "./assets/09n.svg";
+import img10d from "./assets/10d.svg";
+import img10n from "./assets/10n.svg";
+import img11d from "./assets/11d.svg";
+import img11n from "./assets/11n.svg";
+import img13d from "./assets/13d.svg";
+import img13n from "./assets/13n.svg";
+import imgDefault from "./assets/default.svg";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("");
+  const [iconUrl, setIconUrl] = useState("");
+  const [error, setError] = useState(null);
+
+  let dateNow = new Date();
+  let formattedDate = () => {
+    let month = dateNow.toLocaleString("en", { month: "short" });
+    let day = dateNow.getDate();
+    let year = dateNow.getFullYear();
+    let hour = dateNow.getHours();
+    let minute = dateNow.getMinutes();
+    let amOrPm = hour >= 12 ? "pm" : "am";
+
+    let dayWithSuffix =
+      day +
+      (day % 10 === 1 && day !== 11
+        ? "st"
+        : day % 10 === 2 && day !== 12
+        ? "nd"
+        : day % 10 === 3 && day !== 13
+        ? "rd"
+        : "th");
+
+    return `${month} ${dayWithSuffix} ${year} | ${hour}:${
+      minute < 10 ? "0" : ""
+    }${minute} ${amOrPm}`;
+  };
+
+  function fahrToCelsConverter(temp) {
+    return Math.round((temp - 273.15) * 10) / 10;
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        fetchWeatherData(latitude, longitude);
+      },
+      (error) => {
+        console.error("Error getting user location:", error);
+        setError("Error getting user location. Please try again.");
+      }
+    );
+  }, []);
+
+  const fetchWeatherData = (latitude, longitude) => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=519f1ab024e321851b421d162202285d`
+      )
+      .then((res) => {
+        setWeather(res.data);
+        fetchCityName(latitude, longitude);
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response && err.response.status === 429) {
+          setError("Too many requests. Please try again later.");
+        } else {
+          setError("An error occurred. Please try again.");
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+        console.log("finally");
+      });
+  };
+
+  const fetchCityName = (latitude, longitude) => {
+    axios
+      .get(
+        `https://api.api-ninjas.com/v1/reversegeocoding?lat=${latitude}&lon=${longitude}&X-Api-Key=xwXvnaToQoMj9CXmIROYFA==eba1pkNx6FCbgEgn`
+      )
+      .then((res) => {
+        setCity(res.data[0].name);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError("An error occurred. Please try again.");
+      })
+      .finally(() => {
+        setLoading(false);
+        console.log("finally");
+      });
+  };
+
+  useEffect(() => {
+    if (weather.weather && weather.weather.length > 0) {
+      // setIconUrl(
+      //   `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`
+      // );
+      switch (weather.weather[0].icon) {
+        case "01d":
+          setIconUrl(img01d);
+          break;
+        case "01n":
+          setIconUrl(img01n);
+          break;
+        case "02d":
+          setIconUrl(img02d);
+          break;
+        case "02n":
+          setIconUrl(img02n);
+          break;
+        case "03d":
+          setIconUrl(img03d);
+          break;
+        case "03n":
+          setIconUrl(img03n);
+          break;
+        case "04d":
+          setIconUrl(img04d);
+          break;
+        case "04n":
+          setIconUrl(img04n);
+          break;
+        case "09d":
+          setIconUrl(img09d);
+          break;
+        case "09n":
+          setIconUrl(img09n);
+          break;
+        case "10d":
+          setIconUrl(img10d);
+          break;
+        case "10n":
+          setIconUrl(img10n);
+          break;
+        case "11d":
+          setIconUrl(img11d);
+          break;
+        case "11n":
+          setIconUrl(img11n);
+          break;
+        case "13d":
+          setIconUrl(img13d);
+          break;
+        case "13n":
+          setIconUrl(img13n);
+          break;
+        default:
+          setIconUrl(imgDefault);
+      }
+    }
+  }, [weather.weather]);
+
+  if (loading) {
+    return (
+      <div>
+        <p className="loading-text">
+          Please enable location to display local weather.
+        </p>
+        <p className="loading-text">Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  let { temp, temp_min, temp_max } = weather.main;
+  let { description } = weather.weather[0];
+
+  return (
+    <div className="App">
+      <div className="app-container">
+        <div className="title">
+          <h1 className="app-title english">Weather App</h1>
+        </div>
+        <div className="content">
+          <div className="top-part">
+            <div className="city-name">{loading ? "loading.." : city}</div>
+            <div className="date">
+              {loading ? "loading.." : formattedDate()}
+            </div>
+          </div>
+          <div className="body-part">
+            <div className="weather-info">
+              <div className="temperature">
+                <span>
+                  {loading ? "loading.." : fahrToCelsConverter(temp)}°
+                </span>
+              </div>
+              <div className="weather-description">
+                {loading ? "loading.." : description} 🙂
+              </div>
+              <div className="weather-max-min">
+                min: {loading ? "loading.." : fahrToCelsConverter(temp_min)}°{" "}
+                <span className="divider"> | </span>
+                max: {loading ? "loading.." : fahrToCelsConverter(temp_max)}°
+              </div>
+            </div>
+            <div className="weather-illustration">
+              <img className="img" src={iconUrl} alt="weather illustration" />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Created by: Alaa Tahha */}
+      <div className="footer">
+        <p className="footer-text">
+          Created by:{" "}
+          <a
+            href="https://alaatahhan.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            Alaa Tahhan
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
